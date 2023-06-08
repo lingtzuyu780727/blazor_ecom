c30bcc2ddbd3ba539d8b3144542e9a30328de1c8
tzuyuling <tzuyuling@nine-yi.com>
2023-06-08 16:56:14 +0800

    feature: create initial db snapshot

--- ShopOnlineSolution/ShopOnline.Api/Program.cs
+++ ShopOnlineSolution/ShopOnline.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// note 注意AddDbContextPool  (有一群在Pool中) 跟 AddDbContext (每次req就建立) 的差別
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+);
 
 var app = builder.Build();
 
